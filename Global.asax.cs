9ee529f82c046d28f29ecffdfe5932456be5e627
LeNam2111 <135626141+LeNam2111@users.noreply.github.com>
2023-07-18 11:20:41 +0700

    Update Global.asax.cs

--- Global.asax.cs
+++ Global.asax.cs
@@ -16,6 +16,7 @@ namespace QuanLyDiem
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            hhh
         }
     }
 }
